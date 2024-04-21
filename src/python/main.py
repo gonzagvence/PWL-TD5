d2a07c6f9f4cf4db6f9781be28bf45f16677dfc8
Gonza <ggarciavence@gmail.com>
2024-04-21 18:54:16 -0300

    shet

--- src/python/main.py
+++ src/python/main.py
@@ -6,7 +6,7 @@ import time
 def main():
 
 	# Ejemplo para leer una instancia con json
-	instance_name = "optimistic_instance.json"
+	instance_name = "aspen_simulation.json"
 	filename = "././data/" + instance_name
 	with open(filename) as f:
 		instance = json.load(f)
@@ -57,7 +57,6 @@ def main():
 	
 	#-- FUERZA BRUTA ----------------------------------------------------------------------------------------------------
  
-
 	sol = [] #Inicializo Solucion como vacio
 
 	def fuerza_bruta(grid_x, grid_y, x, y, N, sol_parcial):
@@ -124,31 +123,31 @@ def main():
 		return matriz
 	
 	def prog_dinamica(grid_x, grid_y, x, y, N):
-		N = N - 1 
-		Z = len(grid_x)
+		N = N - 1 #Seteamos N en N-1 ya que todos los algoritmos tomamos N como número de Breakpoints.
+		Z = len(grid_x) 
 		M = len(grid_y)
-		memoria = make_cube(N,Z,M)
-		x0 = grid_x[0]
-		for k in range(1,Z):
-			for i in range(M):
+		memoria = make_cube(N,Z,M) # Realizamos un cubo de tamaño NxZxM para almacenar como memoria los resultados parciales
+		x0 = grid_x[0] # Guardamos primer valor de grilla de X para el 'caso base' de la P. Dinámica, donde buscaremos la mejor recta desde X0 Yi a Xj Yl
+		for k in range(1,Z): # #Iteramos grilla de X desde el 1 (ya que el 0 esta fijado en X0)
+			for i in range(M): #Iteramos 2 veces sobre grilla de Y para obtener todos los breakpoints posibles
 				for p in range(M):
-					if memoria[0][k][p]['error'] > estimar_error_y([(x0, grid_y[i]), (grid_x[k], grid_y[p])], x, y):
+					if memoria[0][k][p]['error'] > estimar_error_y([(x0, grid_y[i]), (grid_x[k], grid_y[p])], x, y): #Si es menor al anterior guardado, remplazar
 						memoria[0][k][p] = {'error': estimar_error_y([(x0, grid_y[i]), (grid_x[k], grid_y[p])], x, y),'puntos': [(x0, grid_y[i]), (grid_x[k], grid_y[p])]}
-		for i in range(1,N):
-			for k in range(2,Z):
-				for p in range(M):
-					for l in range(k):
-						for t in range(M):
-							error = memoria[i - 1][l][t]['error']
+		for i in range(1,N): # Agarro desde '2 piezas' para adelante, ya que los valores con 1 sola pieza los calcule en el caso base
+			for k in range(2,Z): # Tomo a partir de la tercer posicion de la grilla de X ya que la de las segunda ya estan calculados
+				for p in range(M): # Itero sobre Grilla de y para probar todos los valores
+					for l in range(k): #Itero sobre rango de K, ya que l nunca puede ser mayor a k. Ya que se tiene que cumplir que los X sean crecientes
+						for t in range(M): #Itero sobre grilla de y para tomar el segundo valor
+							error = memoria[i - 1][l][t]['error'] # Me guardo el error de ir hasta Xl Yt 
 							if(memoria[i][k][p]['error'] > error):
-								error = memoria[i - 1][l][t]['error'] + estimar_error_y([(grid_x[l], grid_y[t]), (grid_x[k], grid_y[p])], x, y)
-								if(memoria[i][k][p]['error'] > error):
+								error = memoria[i - 1][l][t]['error'] + estimar_error_y([(grid_x[l], grid_y[t]), (grid_x[k], grid_y[p])], x, y) # Guardamos error de ir Xk Yp como el anterior calculado más la suma de ir de ese a Xk Yp
+								if(memoria[i][k][p]['error'] > error): #Remplazo solución si es mejor
 									memoria[i][k][p]['error'] = error
 									puntos = memoria[i - 1][l][t]['puntos']
 									puntos.append((grid_x[k], grid_y[p]))
 									memoria[i][k][p]['puntos'] = puntos.copy()
 									puntos.pop()
-		diccionario_menor_error = min(memoria[N - 1][Z - 1], key=lambda x: x["error"])
+		diccionario_menor_error = min(memoria[N - 1][Z - 1], key=lambda x: x["error"]) #Devuelvo el mínimo error llegado la ultima posición de grilla de X, con los breakpoints que pedimos
 		return diccionario_menor_error
 		
 
@@ -160,65 +159,6 @@ def main():
 
 
 	#----------------
-	''' # Experimentando nuestras primeras soluciones
-	def prog_dinamica_(grid_x,grid_y,x,y,N):
-		N = N-1
-		Z = len(grid_x)
-		M = len(grid_y)
-		memoria = make_cube(N, Z, M)
-		x0 = grid_x[0]
-		i = 0
-		p = 0
-		k = 1
-		while k < Z:
-			while i < M:
-				while p < M:
-					if(memoria[0][k][p]['error'] > estimar_error_y([(x0,grid_y[i]),(grid_x[k],grid_y[p])], x, y)):
-						memoria[0][k][p] = {'error':estimar_error_y([(x0,grid_y[i]),(grid_x[k],grid_y[p])], x, y),'puntos':[(x0,grid_y[i]),(grid_x[k],grid_y[p])]}
-					p = p + 1
-				p = 0
-				i = i + 1
-			k = k + 1
-			i = 0
-		i = 1
-		p = 0
-		k = 2
-		l = 0
-		t = 0
-		while i < N:
-			while k < Z:
-				while p < M:
-					while l < k:
-						while t < M:
-							error = memoria[i-1][l][t]['error'] 
-							if(memoria[i][k][p]['error'] > error):
-								error = memoria[i-1][l][t]['error'] + estimar_error_y([(grid_x[l],grid_y[t]), (grid_x[k],grid_y[p])], x, y)
-								if(memoria[i][k][p]['error'] > error):
-									memoria[i][k][p]['error'] = error
-									puntos = memoria[i-1][l][t]['puntos']
-									puntos.append((grid_x[k],grid_y[p]))
-									memoria[i][k][p]['puntos'] = puntos.copy()
-									puntos.pop()
-							t = t + 1
-						l = l + 1
-						t = 0
-					p = p + 1
-					l = 0
-				k = k + 1
-				p = 0
-			i = i + 1
-			k = 2
-
-		diccionario_menor_error = min(memoria[N-1][Z-1], key=lambda x: x["error"])
-		return diccionario_menor_error
- 
-
-	inicio_progri = time.time()
-	print('Programacion Dinámica Bottom Up: ',prog_dinamica_(grid_x,grid_y,x,y,5))
-	fin_progri = time.time()
-	tiempo_progri = fin_progri - inicio_progri
-	print(tiempo_progri)
-	'''
 	print('-------------------------------------')
 	#----------------
 	#Experimento 1: Como varía la calidad de la predicción a medida que aumentamos el tamaño de grilla o cantidad de Breakpoints.
@@ -317,6 +257,7 @@ def main():
 		#---------------------------------
 
 		return
+	
 	#Experimento 2: Performance.
 	#Queremos analizar que variables modifican el rendimiento de nuestros algoritmos. Para esta tarea, vamos a ir variando Tamaño de grilla, cantidad de breakpoints, lenguajes y algoritmos para medir su tiempo de cómputo
 	def exp2():
@@ -475,15 +416,17 @@ def main():
 		fin = time.time()
 		print('Programación Dinámica: ' , fin-inicio, ' segundos')
 		return
-	exp1()
+	
 
 
 
 
 	
 	#-----------------
-	#Ejemplo de como guardamos y exportamos la solución:
-	best = prog_dinamica(grid_x,grid_y,x,y,N)
+	#Ejemplo de como guardamos y exportamos la solución: (elegimos todo lo que queremos de la solución, menos el archivo que está en la parte superior)
+	grid_x = np.linspace(min(instance["x"]), max(instance["x"]), num=20, endpoint=True)
+	grid_y = np.linspace(min(instance["y"]), max(instance["y"]), num=20, endpoint=True)
+	best = prog_dinamica(grid_x,grid_y,x,y,10)
 	best['n'] = len(best['puntos'])
 
 	# Se guarda el archivo en formato JSON
